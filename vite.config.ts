ea652808a361a9635a01c38f07b3fd264d0218a8
Artem Netsvetaev <physphile@vk.com>
2023-08-16 22:31:16 +0300

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import postcssPresetEnv from 'postcss-preset-env';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [vue()],
+	css: {
+		postcss: {
+			plugins: [
+				postcssPresetEnv({
+					features: {
+						'nesting-rules': true,
+					},
+				}),
+			],
+		},
+	},
+});
