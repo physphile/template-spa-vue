538c6a4760212e063da74cc9607736e0cb1d0a53
Netsvetaev Artem <netsvetaev.mgn@gmail.com>
2023-12-09 00:36:01 +0300

    Tuned linters, more YAML!

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,24 @@
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
 import postcssPresetEnv from 'postcss-preset-env';
+import stylelint from 'vite-plugin-stylelint';
+import svgLoader from 'vite-svg-loader';
+import vue from '@vitejs/plugin-vue';
 import vuetify from 'vite-plugin-vuetify';
 
 export default defineConfig({
-	plugins: [vue(), vuetify()],
+	plugins: [
+		vue(),
+		vuetify(),
+		svgLoader({
+			svgoConfig: {
+				floatPrecision: 2,
+				multipass: true,
+			},
+		}),
+		stylelint({
+			files: ['src/**/*.{vue,css}'],
+		}),
+	],
 	css: {
 		postcss: {
 			plugins: [
